3ece624e2538e1a5b2d8049873896a4a9ed6b046
Mitchell Klijs <mitchell1998@gmail.com>
2021-01-01 20:21:48 +0100

    Prevent broadcasting MessageWasSent event when broadcating has been disabled in config

--- src/Eventing/MessageWasSent.php
+++ src/Eventing/MessageWasSent.php
@@ -5,12 +5,11 @@ namespace Musonza\Chat\Eventing;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Musonza\Chat\Models\Message;
 
-class MessageWasSent extends Event implements ShouldBroadcast
+class MessageWasSent extends Event
 {
     use Dispatchable;
     use InteractsWithSockets;
